094bb389e05efa17d031e326d588250ced5da1ed
Augusto Martinez <acm_asd@hotmail.com>
2019-10-18 16:14:29 -0400

    Add files via upload

---
+++ app/js/cpanel.js
@@ -0,0 +1,226 @@
+var counter = 0;
+var keyName;
+var values=[];
+var nodes = [];
+
+function run(){
+	
+	addCompId();
+	addaccuserpass();
+	checkaccuser();
+	getcompdid();
+	checkcompuser();
+	addcompuserpass();
+	
+}
+
+function addCompId(){
+	var elem = document.getElementById("company");
+	elem.addEventListener('keyup',addCompReg,false);		
+}
+
+function addCompReg(){
+	var elemSource = document.getElementById("company");
+	var elemTarget = document.getElementById("compdid");
+  var str;
+	var txt;
+	
+	str = elemSource.value;	
+	str = str.trim();	
+	str =	str.substr(0,str.indexOf(' '));
+	txt = randChar(5);
+	
+	if (str.length > 0 ){
+		str = str.toLowerCase();
+		elemTarget.value = str + txt;
+	}
+	else{
+		str = elemSource.value;	
+		str = str.trim();	
+		str = str.toLowerCase();
+		elemTarget.value = str + txt;
+	}
+	
+}
+
+
+function addaccuserpass(){
+	var elem = document.getElementById("genpass");
+	var elempass = document.getElementById("accpass");
+	
+	elem.addEventListener('click',function(){setPass(elempass);},false);
+}
+
+function addcompuserpass(){
+	var elem = document.getElementById("gencompuserpass");
+	var elempass = document.getElementById("compuserpass");
+		
+	elem.addEventListener('click',function(){setPass(elempass)},false);
+}
+
+function setPass(elem){
+	var pass = randChar(8);
+			
+	elem.value = pass;
+	
+}
+
+//Generate random string
+
+function randChar(len){
+	var txt = "";
+	var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	
+	for(var i = 0; i < len; i++){
+		txt += str.charAt(Math.floor(Math.random()*str.length));
+	}
+	
+	return txt;
+}
+
+
+function addEventListenerList(list, event, func){
+	for (var i = 0, j = 1, len = list.length; i < len; i++, j++){
+		list[i].addEventListener(event, func, false);
+		nodes.push(j);
+	}
+}
+
+function validateaccform(){
+	var elem = document.getElementById("validateaccuser");
+	var str = elem.innerHTML;
+	
+	if(str.length > 0){
+		return false;
+	}
+	
+}
+
+function validatecompuserform(){
+	var elem = document.getElementById("validatecompuser");
+	var str = elem.innerHTML;
+	
+	if(str.length > 0){
+		return false;
+	}
+	
+}
+
+
+function checkaccuser(){
+	var elem = document.getElementById("accuser");
+	elem.addEventListener("blur", reqvalidaccuser, false);	
+}
+
+function reqvalidaccuser(){	
+	var elem = document.getElementById("accuser");
+	var str = elem.value;
+	
+	if(str.length === 0){
+		document.getElementById("validateaccuser").innerHTML = "";
+		return;
+	}
+	
+	else{
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function(){
+				if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+					document.getElementById("validateaccuser").innerHTML = xmlhttp.responseText;
+				}
+			}
+		
+		xmlhttp.open("GET", "inc/cont-checkaccuser.php?usr=" + str, true);
+		xmlhttp.send();		
+	}
+	
+	str = document.getElementById("validateaccuser").innerHTML;
+	
+	if(str.length > 0){
+		
+		var btn = document.getElementById("btn-saveaccuser");
+		disableBtn(btn);
+	
+	}
+	
+}
+
+function getcompdid(){
+	var elem = document.getElementById("addcomplist");
+	
+	if(elem){
+		elem.addEventListener("blur", getcompdidreg, false);	
+	}
+}
+
+function getcompdidreg(){	
+	var elem = document.getElementById("addcomplist");
+	var str = elem.value;
+	
+	if(str.length === 0){
+		document.getElementById("addcompdid").value = "";
+		return;
+	}
+	
+	else{
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function(){
+				if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+					document.getElementById("addcompdid").value = xmlhttp.responseText;
+				}
+			}
+		
+		xmlhttp.open("GET", "inc/cont-getcompdid.php?comp=" + str, true);
+		xmlhttp.send();		
+	}
+	
+	
+}
+
+function checkcompuser(){
+	var elem = document.getElementById("addcompuser");
+	if(elem){
+	 elem.addEventListener("blur", reqvalidaccuser, false);	
+	}
+	
+}
+
+function reqvalidaccuser(){	
+	var elem = document.getElementById("addcompuser");
+	var str = elem.value;
+	
+	if(str.length === 0){
+		document.getElementById("validatecompuser").innerHTML = "";
+		return;
+	}
+	
+	else{
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function(){
+				if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+					document.getElementById("validatecompuser").innerHTML = xmlhttp.responseText;
+				}
+			}
+		
+		xmlhttp.open("GET", "inc/cont-checkcompuser.php?usr=" + str, true);
+		xmlhttp.send();		
+	}
+	
+	str = document.getElementById("validatecompuser").innerHTML;
+	
+	if(str.length > 0){
+		
+		var btn = document.getElementById("btn-savecompuser");
+		disableBtn(btn);
+	
+	}
+	
+	
+}
+
+
+function disableBtn(btn){
+	btn.style = "cursor:not-allowed";
+}
+
+
+window.onload = run;
---
+++ app/js/visit.js
@@ -0,0 +1,76 @@
+var counter = 0;
+var keyName;
+var values=[];
+
+function run(){  
+  additem();
+}
+
+function additem(){  
+  document.getElementById("add-guest").addEventListener('click',addLi,false);   
+}
+
+function addLi(){
+  var maxLi = 6;
+  var liCount;
+  
+  var ul = document.getElementById("guest-list");//get ul element
+  
+  liCount = ul.childNodes.length;
+  
+  var li = document.createElement('li');  
+  
+  if(sessionStorage){
+    counter = sessionStorage.getItem('counter');
+    counter = parseInt(counter);
+    if(!counter){
+      counter = 0;
+    }
+  }
+  
+  counter++;  
+  sessionStorage.setItem('counter',counter);
+  //alert(counter);
+
+  var deleteClick = '<a href="javascript:void(0);" id="item-'+ counter +'" onclick="deleteLi(this.id);" class="delete red-text"><i class="fa fa-times-circle" aria-hidden="true"></i></a>';
+  var newname = '<input id="name-'+ counter +'" type="text" name="names[]" class="form-control" placeholder="Nombres y Apellidos" required>';
+  var newcompany = '<input id="company-'+ counter +'" type="text" name="companies[]" class="form-control" placeholder="Nombre de la compañía" required/>';
+  var newid = '<input id="personal-'+ counter +'" type="text" name="personalids[]" class="form-control" placeholder="Cedula de identidad" required/>';
+  var visitor = '<input id="visitor-'+ counter +'" type="hidden" name="visitortype[]" value="guest"/>';
+  //var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  
+  li.innerHTML = '<span class="sub-wrapper">' + newname + newcompany + newid + visitor  +'</span>' + deleteClick;    
+  
+  if(maxLi > liCount){
+    ul.appendChild(li);    
+  }
+  
+}
+
+
+function deleteLi(btnName){
+  var ul = document.getElementById("guest-list");
+  var a = document.getElementById(btnName);
+  var li = a.parentNode;
+  
+  ul.removeChild(li);
+}
+
+function thischecked(id){
+  var company = "company-"+id;
+  var auth = "auth-"+id;
+  var visitortype = "visitortype-"+id;
+  
+  if(document.getElementById(company).checked === true){
+    document.getElementById(company).checked = false;
+    document.getElementById(auth).checked = false;
+    document.getElementById(visitortype).checked = false;
+  }
+  else{
+    document.getElementById(company).checked = true;
+    document.getElementById(auth).checked = true;
+    document.getElementById(visitortype).checked = true;
+  }  
+}
+
+window.onload = run;
