68014b9964e13b26448213281f12eb54857164e9
Augusto Martinez <acm_asd@hotmail.com>
2019-10-18 16:29:59 -0400

    Create mysqlconnect.php

---
+++ app/inc/mysqlconnect.php
@@ -0,0 +1,16 @@
+<?php
+ 
+$mysql_host = "localhost"; //bd host
+$mysql_database = "database"; //bd name
+$mysql_user = "root"; //bd username
+$mysql_password = "password"; //bd username pass
+ 
+//Open DB
+$conx = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
+ 
+//If connection fails display error message
+if (!$conx) {
+  die ("MySQL connection failed: ".mysqli_connect_errno());
+}
+ 
+?>
