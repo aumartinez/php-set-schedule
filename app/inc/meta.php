37e21a4cfc905fe95684b620fbd32c962edb6ca6
Augusto Martinez <acm_asd@hotmail.com>
2019-10-18 16:13:06 -0400

    Create meta.php

---
+++ app/inc/meta.php
@@ -0,0 +1,20 @@
+<!--Meta tags-->
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="robots" content="noindex, nofollow"/>
+
+<!--JQuery (necessary for Bootstrap's JavaScript plugins)-->
+<script src="/sched/js/jquery.min.js" type="text/javascript"></script>
+<!--Bootstrap compiled plugin-->
+<script src="/sched/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/sched/js/moment.js" type="text/javascript"></script>
+<script src="/sched/js/bootstrap-datetimepicker.js" type="text/javascript"></script>
+
+<!--Stylesheets-->
+<link href="/sched/css/bootstrap.min.css" rel="stylesheet" />
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" />
+<link href="/sched/css/bootstrap-datetimepicker.css" rel="stylesheet" />
+<link href="/sched/css/mycss.css" rel="stylesheet" />
+
+<?php echo "\n"; ?>
