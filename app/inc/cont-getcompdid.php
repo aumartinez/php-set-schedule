15495a03cbf6fa4bb5db7e9f88accb24f4330e5a
Augusto Martinez <acm_asd@hotmail.com>
2019-10-21 16:01:51 -0400

    Create cont-getcompdid.php

---
+++ app/inc/cont-getcompdid.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+$logged = $_SESSION['loggedstaff'];
+
+if(!$logged){
+	header('Location:loggedout.php');
+	exit();
+}
+
+if(isset($_GET['comp'])){
+  $compname = $_GET['comp'];
+  
+  include 'mysqlconnect.php';
+  
+  $tablename = "companies";
+  $sql = "SELECT *
+  FROM $tablename
+  WHERE fullcompany = '$compname'";
+  
+  $query = mysqli_query($conx, $sql);
+  
+  if(!$query){
+    die("Error :".mysqli_error($conx));
+  }
+  
+  $rows = mysqli_num_rows($query);  
+  
+  if($rows == 1){
+    $result = mysqli_fetch_assoc($query);
+		echo $result['compdid'];
+  }
+  
+	mysqli_free_result($query);
+  mysqli_close($conx);
+  
+}
+
+
+?>
