c6384e614b2840aaaada1cc7c91c6a587ab26cdb
Augusto Martinez <acm_asd@hotmail.com>
2019-10-22 11:21:44 -0400

    Update startup.php

--- startup.php
+++ startup.php
@@ -46,6 +46,10 @@ else {
 <!doctype html>
 <html>
   <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    
     <title>
       Setup start database setup
     </title>
