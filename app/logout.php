9f2f2146eef2bbf37484780ca5c1b641b2b7c6cb
Augusto Martinez <acm_asd@hotmail.com>
2019-10-24 15:20:51 -0600

    Update logout.php

--- app/logout.php
+++ app/logout.php
@@ -6,8 +6,6 @@ session_start();
 unset($_SERVER['PHP_AUTH_USER']);
 unset($_SERVER['PHP_AUTH_PW']);
 
-header('HTTP/1.0 401 Unauthorized');
-
 //Clear any session variable
 session_unset();
 
@@ -57,9 +55,7 @@ session_destroy();
     }
 
     countdown();
-  </script>
-  
-<?php include 'inc/bottomscripts.php'; ?>
+  </script>  
 
 </body>
 </html>
