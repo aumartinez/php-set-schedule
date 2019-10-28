250c25fb3bf9549c225b0559264526c00cb4cda7
Augusto Martinez <acm_asd@hotmail.com>
2019-10-28 18:26:43 -0400

    Create index.php

---
+++ app/inc/index.php
@@ -0,0 +1,14 @@
+<?php
+  header('HTTP/1.0 401 Unauthorized');
+  exit();
+?>
+<!doctype html>
+<html>
+  <head>
+    <title>
+      Blank page
+    </title>
+  </head>
+  <body>    
+  </body>
+</html>
